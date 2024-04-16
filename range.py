f2ba8aaa01f60b76704fdd2518de81edf87ba7ee
vaibhavrakeshpatel <166784492+vaibhavrakeshpatel@users.noreply.github.com>
2024-04-16 14:57:06 +0530

    Add files via upload
    check the time table of train for particular day

---
+++ range.py
@@ -0,0 +1,186 @@
+print("Welcome to Indian Railways")
+print("you are at MUMBAI CENTRAL station")
+print("Trains at the glance")
+print("we wish you safe and happy journey")
+print("Keep the station and surroundings clean")
+print("please travel with the valid ticket")
+print("helpline number incase of any emergency is 139")
+print("coaches and their status")
+print("Eng: Engine")
+print("SLR: Luggage and break van")
+print("Gen:  second class Non - reservation coach")
+print("S: reservation coach  Non - AC Sleeper")
+print("B: reservation AC 3 Tier coach Sleeper")
+print("A: reservation AC 2 Tier coach Sleeper")
+print("HA: reservation AC 1 class coach Sleeper")
+print("PC: pantry coach for food available")
+print("GD: Guard coach")
+print("C: reservation AC chair car coach sitting")
+print("D: reservation chair car coach sitting")
+print("E: reservation Executive chair car coach sitting")
+print("all trains between")
+print("")
+name: str = "Ahmedabad exp"
+number: int  = 23456
+number1: int = 0o1
+depature: float = 20.45
+
+number3: str=  "Eng","SLR","gen1","Gen2","S1","S2","S3","S4","S5","S6","S7","S8","S9","S10","S11","S12","A1","A2","B1","B2","B3","HA1","Gen3","Gen4","GD"
+print(f" Train { number } is {name} on platform {number1}  ")
+print(f"coach position is {number3}. ")
+print(f"depature time is{depature}pm")
+
+
+name: str ="goa exp"
+number: int = 36780
+number2: int = 0o2
+depature1: float = 20.12
+
+number4: str = "Eng","SLR","Gen1","HA1","A2","A1","B4","B3","B2","B1","S10","S9","S8","S7","S6","S5","S4","S3","S2","S1","Gen2","gen3","GD"
+print(f" Train {number} is {name} on platform {number2} ")
+print(f"coach position is {number4}. ")
+print(f"depature time is {depature1}pm")
+
+
+name: str = "Chennai mail"
+number: int  = 48960
+number5: int = 0o3
+depature2: float = 21.05
+
+number6: str = "Eng","SLR","Gen1","HA1","A2","A1","B4","B3","B2","B1","PC","S10","S9","S8","S7","S6","S5","S4","S3","S2","S1","Gen2","GD"
+print(f" Train {number} is {name} on platform {number5} ")
+print(f"coach position is {number6}. ")
+print(f"depature time is {depature2}pm")
+
+
+name: str = "Punjab mail"
+number: int  = 55823
+number6: int = 0o4
+depature3: float = 21.35
+
+number7: str = "Eng","SLR","Gen1","Gen2","HA1","A2","A1","B3","B2","B1","PC","S10","S9","S8","S7","S6","S5","S4","S3","S2","S1","Gen3","Gen4","GD"
+print(f" Train {number} is {name} on platform {number6} ")
+print(f"coach position is {number7}. ")
+print(f"depature time is {depature3}pm")
+
+
+name: str = "howrah Mail"
+number: int  = 68236
+number7: int = 0o5
+depature4: float = 19.35
+
+number8: str = "Eng","SLR","Gen1","Gen2","S9","S8","S7","S6","S5","S4","S3","S2","PC","S1","A1","A2","B1","B2","B3","HA1","Gen3","Gen4","GD"
+print(f" Train {number} is {name} on platform {number7} ")
+print(f"coach position is {number8}. ")
+print(f"depature time is {depature4}pm")
+
+
+name: str = "hyderabad Exp"
+number: int = 76541
+number8: int = 0o6
+depature5: float = 21.55
+
+number9: str = "Eng","SLR","Gen1","Gen2","S1","S2","S3","S4","S5","S6","S7","B1","B2","B3","B4","A3","A2","A1","Gen3","Gen4","GD"
+print(f" Train {number} is {name} on platform {number8}")
+print(f" coach position is {number9}")
+print(f" depature time is {depature5}pm")
+
+
+name: str = "Banglore Exp"
+number: int = 89071
+number9: int = 0o7
+depature6: float = 22.15
+
+number10: str = "Eng","SLR","Gen1","Gen2","Gen3","Gen4","S1","S2","S3","S4","S5","S6","S7","S8","S9","S10","PC","B1","B2","B3","B4","A2","A1","GD"
+print(f" Train {number} is {name} on platform {number9}")
+print(f" coach position is {number10}")
+print(f" depature time is {depature6}pm")
+
+
+name: str = "Pune Exp"
+number: int = 90623
+number10: int = 8
+depature7: float = 19.03
+
+number9: str = "Eng","SLR","Gen1","Gen2","Gen3","Gen4","D1","D2","D3","D4","D5","D6","C1","C2","C3","Gen5","Gen6","Gen7","GD"
+print(f" Train {number} is {name} on platform {number10} ")
+print(f"coach position is {number9}. ")
+print(f"depature time is {depature7}pm")
+
+
+name: str = "Rajdhani Exp"
+number: int = 14596
+number11: int = 9
+depature8: float = 17.00
+
+number10: str = "Eng","SLR","B1","B2","B3","B4","B5","B6","B7","B8","B9","B10","B11","PC","HA1","A1","A2","A3","A4","A5","GD"
+print(f" Train {number} is {name} on platform {number11} ")
+print(f"coach position is {number10}. ")
+print(f"depature time is {depature8}pm")
+
+
+name: str = "Varanasi Exp"
+number: int = 12900
+number12: int = 10
+depature9: float = 18.22
+
+number11: str = "Eng","SLR","B1","B2","B3","B4","B5","B6","B7","B8","B9","B10","B11","PC","HA1","A1","A2","A3","A4","A5","GD"
+print(f" Train {number} is {name} on platform {number12} ")
+print(f"coach position is {number11}. ")
+print(f"depature time is {depature9}pm")
+
+name: str = "Ernakulam  Exp "
+number: int = 13457
+number13: int = 9
+depature10: float = 22.35
+
+number12: str = "Eng","SLR","Gen1","Gen2","S12","S11","S10","S9","S8","S7","S6","S5","S4","S3","S2","S1","B2","B1","A1","Gen3","Gen4","GD"
+print(f" Train {number} is {name} on platform {number13} ")
+print(f"coach position is {number12}. ")
+print(f"depature time is {depature10}pm")
+
+name: str = "Jaipur Duranto  Exp "
+number: int = 15767
+number14: int = 10
+depature11: float = 23.00
+
+number13: str = "Eng","SLR","B8","B7","B6","B5","B4","B3","B2","B1","PC","A4","A3","A2","A1","HA1","GD"
+print(f" Train {number} is {name} on platform {number14} ")
+print(f"coach position is {number13}. ")
+print(f"depature time is {depature11}pm")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
